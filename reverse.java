2bfe3a8a785c174ffe913ea1064b3d3118753837
Saurav kumar Arya <92774037+sauravkrarya@users.noreply.github.com>
2024-04-17 22:37:22 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -1,20 +1,16 @@
-import java.util.Scanner;
-public class reverse {
-    public static void main(String[] args) {
-        int rev=0,rem=0;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter the number");
-        int num=sc.nextInt();
-        while (num>0){
-        rem=num%10;
-        System.out.println("rem"+ rem);
-        rev=(rev*10)+rem;
-        System.out.println("rev:"+ rev);
-        num=num/10;
-        System.out.println("num:"+ num) ;
-          
-      }
-   }
-  }
+import java.util.*;
+class reverse{
+
+public static void main(String[] args)
+{
+    // int arr[] = {1, 2, 3, 4, 5, 6};
+    String arr="Saurav";
+     int n = arr.length();
+
+    for(int i=n-1; i>=0; i--){
+        System.out.print(arr.charAt(i));
+    }
     
 
+}
+}
